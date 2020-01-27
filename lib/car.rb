bed5366365dfd39b4a0a4e91f27a72058ead69a5
Pledge Davis <pledgedavis14@gmail.com>
2020-01-27 23:03:07 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -2,9 +2,10 @@ require_relative "./vehicle.rb"
 class Car
 attr_accessor :wheel_size, :wheel_number
  def initialize(wheel_size, wheel_number)
+   @wheel_size = wheel_size
+   @wheel_number = wheel_number
 
-
-
+ end
 
 
 
