aed1eee6d47875930209476ecf1f1598c95baf7c
Pledge Davis <pledgedavis14@gmail.com>
2020-01-27 22:47:30 +0000

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,25 @@
 require_relative "./vehicle.rb"
+class Car
+  attr_accessor :wheel_size, :wheel_number
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end
+
+def go
+return 'VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!'
+
+end
+
+def fill_up_tank
+
+return 'filling up!'
+
+end
+
+
+
+
+
+
+end
